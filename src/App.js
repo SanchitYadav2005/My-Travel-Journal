390cc9709e8ac7d37c8bcaaf6b11eef9daa57345
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-20 10:38:10 +0530

    done with making navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import style from "./App.css"
+
+
+export default function App(){
+  return(
+    <div>
+      <Navbar />
+    </div>
+  )
+}
