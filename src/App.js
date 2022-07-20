2824bbd8fa97500c45c566caf36c578fa1cb3d58
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-07-20 10:45:44 +0530

    added some data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import style from "./App.css"
+import data from './data'
 
 
 export default function App(){
+  const cards = data.map(iteams =>{
+    return(
+      <Card 
+        location= {iteams.location}
+      />
+    )
+  })
   return(
     <div>
       <Navbar />
+      {cards}
     </div>
   )
 }
